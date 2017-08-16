b41435c1f43671bae61a786319448e3f48ae1d5d
Jay Cheroske <jay@portal.org>
2017-08-15 21:18:55 -0700

    Deleted functions

--- src/functions/getActiveUsers.js
+++
@@ -1,5 +0,0 @@
-export default (data) => {
-  if (data == null || data.users == null) return null
-
-  return data.users.filter(u => u.accountActive === true)
-}
--- src/functions/getProductById.js
+++
@@ -1,5 +0,0 @@
-export default (data, id) => {
-  if (data == null || data.products == null || id == null) return null
-
-  return data.products.find(u => u.id === id)
-}
